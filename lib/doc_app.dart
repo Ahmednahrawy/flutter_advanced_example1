db5d4eea6e7c46b5ef6b9747048c2798a2819cfd
Ahmad.Alnahrawy <37123037+Ahmednahrawy@users.noreply.github.com>
2024-07-23 14:44:47 +0300

    first work onboarding screen

---
+++ lib/doc_app.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_advanced_example1/core/routing/app_router.dart';
+import 'package:flutter_advanced_example1/core/routing/routes.dart';
+import 'package:flutter_advanced_example1/core/theming/colors.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocApp extends StatelessWidget {
+  const DocApp({super.key, required this.appRouter});
+  final AppRouter appRouter;
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Doc App',
+        theme: ThemeData(
+          primaryColor: ColorsManager.mainBlue,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        initialRoute: Routes.onBoardingScreen,
+        onGenerateRoute: appRouter.generateRoute,
+      ),
+    );
+  }
+}
