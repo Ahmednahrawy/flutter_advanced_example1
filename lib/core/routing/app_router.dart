fba47322adb1cdd1f5fb436e0f40ffc22bf95c93
Ahmad.Alnahrawy <37123037+Ahmednahrawy@users.noreply.github.com>
2024-08-23 17:55:12 +0300

    - add font weight helper in theming folder - login screen design - add to colors file - add to styles file - add text_form_field.dart in widgets folder, core - add textButton file in widgets folder, core - add new routes to routes file

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_example1/core/routing/routes.dart';
-import 'package:flutter_advanced_example1/features/login/ui/widgets/login_screen.dart';
+import 'package:flutter_advanced_example1/features/login/ui/login_screen.dart';
 import 'package:flutter_advanced_example1/features/onboarding/onboarding_screen.dart';
 
 class AppRouter {
